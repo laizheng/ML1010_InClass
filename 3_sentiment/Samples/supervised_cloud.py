ad72288983bca3f97d6f2c948ee7a324af6db5d0
James Lai <gdlaizheng@gmail.com>
2018-11-17 23:33:14 -0500

    added comments

--- 3_sentiment/Samples/supervised_cloud.py
+++ 3_sentiment/Samples/supervised_cloud.py
@@ -16,7 +16,7 @@ print(dataset.head())
 reviews = np.array(dataset['review'])
 sentiments = np.array(dataset['sentiment'])
 
-# build train and test datasets
+print("#build train and test datasets")
 norm_train_reviews = reviews[:35000]
 norm_train_sentiments = sentiments[:35000]
 norm_test_reviews = reviews[35000:]
@@ -24,19 +24,19 @@ norm_test_sentiments = sentiments[35000:]
 
 le = LabelEncoder()
 num_classes=2
-# tokenize train reviews & encode train labels
+print("#tokenize train reviews & encode train labels")
 tokenized_train = [tn.tokenizer.tokenize(text)
                    for text in norm_train_reviews]
 y_tr = le.fit_transform(norm_train_sentiments)
 y_train = keras.utils.to_categorical(y_tr, num_classes)
-# tokenize test reviews & encode test labels
+print("#tokenize test reviews & encode test labels")
 tokenized_test = [tn.tokenizer.tokenize(text)
                    for text in norm_test_reviews]
 y_ts = le.fit_transform(norm_train_sentiments)
 y_test = keras.utils.to_categorical(y_ts, num_classes)
 
-# Feature Engineering with word embeddings
-# build word2vec model
+print("#Feature Engineering with word embeddings")
+print("#build word2vec model")
 w2v_num_features = 500
 w2v_model = gensim.models.Word2Vec(tokenized_train, size=w2v_num_features, window=150,
                                    min_count=10, sample=1e-3)
@@ -61,13 +61,13 @@ def averaged_word2vec_vectorizer(corpus, model, num_features):
                     for tokenized_sentence in corpus]
     return np.array(features)
 
-# generate averaged word vector features from word2vec model
+print("#generate averaged word vector features from word2vec model")
 avg_wv_train_features = averaged_word2vec_vectorizer(corpus=tokenized_train, model=w2v_model,
                                                      num_features=500)
 avg_wv_test_features = averaged_word2vec_vectorizer(corpus=tokenized_test, model=w2v_model,
                                                     num_features=500)
 
-# feature engineering with GloVe model
+print("#feature engineering with GloVe model")
 train_nlp = [tn.nlp(item) for item in norm_train_reviews]
 train_glove_features = np.array([item.vector for item in train_nlp])
 
@@ -77,7 +77,7 @@ test_glove_features = np.array([item.vector for item in test_nlp])
 print('Word2Vec model:> Train features shape:', avg_wv_train_features.shape, ' Test features shape:', avg_wv_test_features.shape)
 print('GloVe model:> Train features shape:', train_glove_features.shape, ' Test features shape:', test_glove_features.shape)
 
-#Building Deep neural network architecture
+print("#Building Deep neural network architecture")
 def construct_deepnn_architecture(num_input_features):
     dnn_model = Sequential()
     dnn_model.add(Dense(512, activation='relu', input_shape=(num_input_features,)))
@@ -95,6 +95,7 @@ def construct_deepnn_architecture(num_input_features):
 
 w2v_dnn = construct_deepnn_architecture(num_input_features=500)
 
+print("#dnn w2v starting.....")
 batch_size = 100
 w2v_dnn.fit(avg_wv_train_features, y_train, epochs=5, batch_size=batch_size,
             shuffle=True, validation_split=0.1, verbose=1)
@@ -102,12 +103,17 @@ y_pred = w2v_dnn.predict_classes(avg_wv_test_features)
 predictions = le.inverse_transform(y_pred)
 meu.display_model_performance_metrics(true_labels=test_sentiments, predicted_labels=predictions,
                                       classes=['positive', 'negative'])
+print("#dnn w2v ended.\n\n")
+
 
 glove_dnn = construct_deepnn_architecture(num_input_features=300)
 batch_size = 100
+
+print("#dnn glove starting.....")
 glove_dnn.fit(train_glove_features, y_train, epochs=5, batch_size=batch_size,
               shuffle=True, validation_split=0.1, verbose=1)
 y_pred = glove_dnn.predict_classes(test_glove_features)
 predictions = le.inverse_transform(y_pred)
 meu.display_model_performance_metrics(true_labels=test_sentiments, predicted_labels=predictions,
                                       classes=['positive', 'negative'])
+print("#dnn glove ended\n\n")
